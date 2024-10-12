6f683065a95cf09c885551ff058294b0e87ebafc
dillonteachescoding <dillon.kierce@greenriver.edu>
2024-10-11 18:45:47 -0700

    woohoo

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,17 @@
-#!/usr/bin/env python3
-# A simple "Hello World" script in python
-# Created 
+#We want to create a script that takes user input
+#and outputs hello there name, as well as have a good
+#day
+
+name = input("Hello there, what is your name? ")
+
+print(f"Hello, {name}")
+
+print(f"Today is going to be a great day. {name}")
+
+# Prompt user for there age
+age = int(input("How old are you: "))
+
+future_age = age + 2
+
+
+print(f"You are {future_age} in two years")
--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+
+def get_input():
+    while True:
+        response = input("Is today a great day? (y/n): ".lower())
+        if response in ['y', 'n']:
+            return response
+        else:
+            print("invalid response please print y or n")
+
+# Get input and check response
+response = get_input()
+
+if response == 'y':
+    for _ in range(10):
+        print("yes it is")
+else:
+    print("today is not the best day")
